00e3fa882b01aace04542c7c2b68242f09c5dba4
Adam Miranda <adamballimiranda@mail.weber.edu>
2019-06-04 18:13:28 -0600

    Add project files.

---
+++ PiePizzaria3/Data/PiePizzaContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PiePizzaria3.Models;
+
+namespace PiePizzaria3.Data
+{
+    public class PiePizzaContext : DbContext
+    {
+        public PiePizzaContext(DbContextOptions<PiePizzaContext> options) : base(options)
+        {
+        }
+        public DbSet<Pie> Pies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pie>().ToTable("Pie");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# PiePizzaria2
+This project is a duplicate of the original PiePizzaria. However, it utilizes Razor pages.
+It took me extra time to learn how to use controllers in Razor.
